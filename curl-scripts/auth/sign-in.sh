efe56d0f823bd1ea61574e7567bf97ae29cccb92
tslilpress <tslilpress@gmail.com>
2020-08-27 11:12:04 -0400

    Added auth folder to curl-scripts and sh files
    - Added auth folder in curl-scripts
    - added sign-up/in/out and change password.sh to auth folder
    - added the curl scripts urls to each files

---
+++ curl-scripts/auth/sign-in.sh
@@ -0,0 +1,17 @@
+'use strict'
+
+API="https://tic-tac-toe-api-development.herokuapp.com"
+URL_PATH="/sign-in"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
---
+++ curl-scripts/auth/sign-out.sh
@@ -0,0 +1,11 @@
+'use strict'
+
+API="https://tic-tac-toe-api-development.herokuapp.com"
+URL_PATH="/sign-out"
+
+curl "${API}${URL_PATH}/" \
+  --include \
+  --request DELETE \
+  --header "Authorization: Token token=${TOKEN}"
+
+echo
