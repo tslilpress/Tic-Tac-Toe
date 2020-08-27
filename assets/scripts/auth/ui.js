d55533576ac6291e42da8995124747456c70f8ef
tslilpress <tslilpress@gmail.com>
2020-08-27 14:03:55 -0400

    Added onSignUpSuccess/Failure to ui.js
    - added onSignUpSuccess/Failure message in ui.js
    - set up export for onSignUpSuccess/Failure in ui.js
    - set up require for store.js in ui.js
    - added form for sign-in in index.html

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,1 +1,14 @@
+const store = require('./../store')
 
+const onSignUpSuccess = function (res) {
+  $('#message').text('Thanks for Siging up ' + res.user.email)
+}
+
+const onSignUpFailure = function (res) {
+  $('#message').text('Oops, something went wrong. Try again.')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure
+}
